debd07474bacad653678c1a179ac8f1ced264c0b
ppinchuk <ppinchuk@nrel.gov>
2022-05-27 16:35:23 -0600

    Added `display_selected_tab_above_map` callback

--- reView/components/callbacks.py
+++ reView/components/callbacks.py
@@ -31,7 +31,7 @@ def toggle_reverse_color_button_style(id_prefix):
         Output(f"{id_prefix}_rev_color", "style"),
         Input(f"{id_prefix}_rev_color", "n_clicks"),
     )
-    def _toggle_reverse_color_button(click):
+    def _toggle_reverse_color_button_style(click):
         """Toggle Reverse Color on/off."""
         if not click:
             click = 0
@@ -44,4 +44,44 @@ def toggle_reverse_color_button_style(id_prefix):
 
         return children, style
 
-    return _toggle_reverse_color_button
+    return _toggle_reverse_color_button_style
+
+
+def display_selected_tab_above_map(id_prefix):
+    """Display the selected tab above the map.
+
+    This method assumes you have all the elements added by
+    `reView.components.divs.above_map_options_div` somewhere
+    in your layout.
+
+    Parameters
+    ----------
+    id_prefix : str
+        A string representing the prefix of the button. This prefix
+        should match the one used for the
+        `reView.components.divs.above_map_options_div` function call.
+
+    Returns
+    -------
+    callable
+        A callable function used by dash. Users should NOT invoke this
+        function themselves.
+    """
+
+    @app.callback(
+        Output(f"{id_prefix}_state_options_div", "style"),
+        Output(f"{id_prefix}_region_options_div", "style"),
+        Output(f"{id_prefix}_basemap_options_div", "style"),
+        Output(f"{id_prefix}_color_options_div", "style"),
+        Input(f"{id_prefix}_options_tab", "value"),
+    )
+    def _display_selected_tab_above_map(tab_choice):
+        """Choose which map tabs to display."""
+        # Styles
+        styles = [{"display": "none"}] * 4
+        order = ["state", "region", "basemap", "color"]
+        idx = order.index(tab_choice)
+        styles[idx] = {"width": "100%", "text-align": "center"}
+        return styles[0], styles[1], styles[2], styles[3]
+
+    return _display_selected_tab_above_map
--- reView/pages/reeds/controller/callbacks.py
+++ reView/pages/reeds/controller/callbacks.py
@@ -15,7 +15,10 @@ from dash.dependencies import Input, Output
 
 from reView.app import app
 
-from reView.components.callbacks import toggle_reverse_color_button_style
+from reView.components.callbacks import (
+    toggle_reverse_color_button_style,
+    display_selected_tab_above_map,
+)
 from reView.components.map import Map
 from reView.pages.reeds.model import cache_reeds
 from reView.utils import calls
@@ -24,6 +27,7 @@ from reView.utils.functions import format_capacity_title
 logger = logging.getLogger(__name__)
 COMMON_CALLBACKS = [
     toggle_reverse_color_button_style(id_prefix="map_reeds"),
+    display_selected_tab_above_map(id_prefix="map_reeds"),
 ]
 
 
