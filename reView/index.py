b2b2e6f49d25f6e916779fe8d40f10b46c71d8d8
ppinchuk <ppinchuk@nrel.gov>
2022-05-27 10:58:22 -0600

    Formatting

--- reView/index.py
+++ reView/index.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=unused-import
 """Transition reView project index file."""
-import reView.pages.scenario.controller.callbacks  # pylint: disable=unused-import
-import reView.pages.reeds.controller.callbacks  # pylint: disable=unused-import
+import reView.pages.scenario.controller.callbacks
+import reView.pages.reeds.controller.callbacks
 
-from reView.app import app, server  # pylint: disable=unused-import
+from reView.app import app, server
 from reView.environment.settings import (
     APP_HOST,
     APP_PORT,
     DASH_DEBUG,
     LOG_LEVEL,
 )
-from reView.routes import render_page_content  # pylint: disable=unused-import
+from reView.routes import render_page_content
 from reView.utils.log import init_logger, log_versions
 
 
@@ -18,11 +19,8 @@ def main():
     """Run reView."""
     init_logger(level=LOG_LEVEL)
     log_versions()
-    app.run_server(
-        host=APP_HOST,
-        port=APP_PORT,
-        debug=DASH_DEBUG
-    )
+    app.run_server(host=APP_HOST, port=APP_PORT, debug=DASH_DEBUG)
+
 
 if __name__ == "__main__":
     main()
