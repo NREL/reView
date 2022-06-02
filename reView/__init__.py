2d99a75f5adbf5dfccd5a8590f0512b4b172a82d
ppinchuk <ppinchuk@nrel.gov>
2022-06-01 19:29:07 -0600

    Added pint for units handling

--- reView/__init__.py
+++ reView/__init__.py
@@ -2,9 +2,18 @@
 """The reV model viewer (reView)"""
 import os
 
+
+from pint import UnitRegistry
+
 from reView.version import __version__
 
 REVIEW_DIR = os.path.dirname(os.path.realpath(__file__))
 REVIEW_DATA_DIR = os.path.join(REVIEW_DIR, "data")
 REVIEW_CONFIG_DIR = os.path.join(os.path.dirname(REVIEW_DIR), "configs")
 TEST_DATA_DIR = os.path.join(os.path.dirname(REVIEW_DIR), "tests", "data")
+
+# May want to move this somewhere in utils
+UNITS = UnitRegistry()
+Q_ = UNITS.Quantity
+UNITS.define("percent = [percent] = % = pct")
+UNITS.define("category = 1 = _ = cat")
