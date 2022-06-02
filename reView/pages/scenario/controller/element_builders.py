f743c14e1cabba35a31ef5a965253587b5ceb748
ppinchuk <ppinchuk@nrel.gov>
2022-06-02 16:44:03 -0600

    linter updates

--- reView/pages/scenario/controller/element_builders.py
+++ reView/pages/scenario/controller/element_builders.py
@@ -11,6 +11,7 @@ Created on Fri May 20 12:07:29 2022
 import copy
 import json
 from collections import Counter
+from itertools import cycle
 
 import pandas as pd
 import numpy as np
@@ -26,14 +27,42 @@ CHART_LAYOUT = copy.deepcopy(DEFAULT_LAYOUT)
 CHART_LAYOUT.update({"legend_title_font_color": "black"})
 
 
-def is_integer(x):
-    """Check if an input is an integer."""
+def _fix_doubles(df):
+    """Check and/or fix columns names when they match."""
+    if not isinstance(df, pd.core.frame.Series):
+        cols = np.array(df.columns)
+        counts = Counter(cols)
+        for col, count in counts.items():
+            if count > 1:
+                idx = np.where(cols == col)[0]
+                cols[idx[1]] = col + "_2"
+        df.columns = cols
+    return df
+
+
+def _is_integer(val):
+    """Check if an input value is an integer."""
     try:
-        int(x)
-        check = True
+        int(val)
+        return True
     except ValueError:
-        check = False
-    return check
+        return False
+
+
+def _add_bins(df, x_var, bins=100, bin_col_name="x_bin"):
+    """Add x bins to df."""
+    # Assign bins as max bin value
+    df = df.dropna(subset=x_var)
+    df = df.sort_values(x_var)
+    min_x, max_x = df[x_var].min(), df[x_var].max()
+    bin_size = np.ceil(max_x - min_x / bins)
+    bins = np.arange(min_x, max_x + bin_size, bin_size)
+
+    df[bin_col_name] = bins[-1]
+    for bin_ in bins[::-1]:
+        df[bin_col_name][df[x_var] <= bin_] = bin_
+
+    return df
 
 
 class Plots:
@@ -62,77 +91,34 @@ class Plots:
         """Print representation string."""
         return f"<Plots object: project={self.config.project}>"
 
-    # def category_check(self):
-    #     """Check for json dictionary entries and adjust if needed."""
-    #     # Use one dataset to check
-    #     sample_df = self.datasets[next(iter(self.datasets))]
-    #     y = sample_df.columns[1]
-    #     if Categories.is_json(sample_df, y):
-    #         adjusted_datasets = {}
-    #         for key, df in self.datasets.items():
-    #             df = df.copy()
-    #             df = self.adjust_category(df)
-    #             adjusted_datasets[key] = df
-    #         self.datasets = adjusted_datasets
-
-    # def adjust_category(self, df):
-    #     """Adjust dataset for categorical data."""
-    #     # We'll need x and y
-    #     x, y = df.columns[:2]
-    #     if Categories.is_json(df, y):
-    #         df[y] = df[y].apply(json.loads)
-    #         df["gid_counts"] = df["gid_counts"].apply(json.loads)
-
-    #     # Find the mode and counts
-    #     df["y_mode"] = Categories().mode(df, y)
-    #     y_counts = Categories().counts(df, y)
-    #     x_portions = {}
-
-    #     # Speed this up?
-    #     arg_list = [(df, y, x, k) for k in y_counts]
-    #     for args in tqdm(arg_list):
-    #         ckey, xp = self._xportions(args)
-    #         x_portions[ckey] = xp
-
-    #     # Make the category dataframe
-    #     adf = pd.DataFrame({y: y_counts, x: x_portions})
-    #     adf["y_mode"] = adf.index
-
-    #     # Append to mode field!
-    #     del df[y]
-    #     del df[x]
-    #     df = pd.merge(df, adf, on="y_mode")
-
-    #     return df
-
-    def _plot_range(self, y):
+    def _plot_range(self, var):
         """Get plot range."""
-        # User defined y-axis limits
+
         user_ymin, user_ymax = self.user_scale
         scale = self.config.scales.get(
-            DiffUnitOptions.remove_from_variable_name(y), {}
+            DiffUnitOptions.remove_from_variable_name(var), {}
         )
         ymin = user_ymin or scale.get("min")
         ymax = user_ymax or scale.get("max")
 
         if ymin and not ymax:
-            ymax = max([df[y].max() for df in self.datasets.values()])
+            ymax = max([df[var].max() for df in self.datasets.values()])
         if ymax and not ymin:
-            ymin = min([df[y].min() for df in self.datasets.values()])
+            ymin = min([df[var].min() for df in self.datasets.values()])
         return [ymin, ymax]
 
-    def _axis_title(self, variable):
+    def _axis_title(self, var):
         """Make a title out of variable name and units."""
-        diff = DiffUnitOptions.from_variable_name(variable)
+        diff = DiffUnitOptions.from_variable_name(var)
         is_difference = diff is not None
         is_percent_difference = diff == DiffUnitOptions.PERCENTAGE
-        variable = DiffUnitOptions.remove_from_variable_name(variable)
-        variable = variable.removesuffix("_2")
-        title = [self.config.titles.get(variable, convert_to_title(variable))]
+        var = DiffUnitOptions.remove_from_variable_name(var)
+        var = var.removesuffix("_2")
+        title = [self.config.titles.get(var, convert_to_title(var))]
 
         if is_percent_difference:
             title += ["(%)"]
-        elif units := self.config.units.get(variable):
+        elif units := self.config.units.get(var):
             title += [f"({units})"]
 
         if is_difference:
@@ -143,33 +129,32 @@ class Plots:
 
         return " ".join(title)
 
-    def cumulative_sum(self, x, y):
-        """Return a cumulative capacity scatterplot."""
+    def cumulative_sum(self, x_var, y_var):
+        """Return a cumulative capacity scatter plot."""
         main_df = None
         for key, df in self.datasets.items():
-            df = self._fix_doubles(df)
+            df = _fix_doubles(df)
             if main_df is None:
                 main_df = df.copy()
-                main_df = main_df.sort_values(y)
-                main_df["csum"] = main_df[x].cumsum()
+                main_df = main_df.sort_values(y_var)
+                main_df["cumsum"] = main_df[x_var].cumsum()
                 main_df[self.GROUP] = key
             else:
-                df = df.sort_values(y)
-                df["csum"] = df[x].cumsum()
+                df = df.sort_values(y_var)
+                df["cumsum"] = df[x_var].cumsum()
                 df[self.GROUP] = key
                 main_df = pd.concat([main_df, df])
 
-        x_title, y_title = self._axis_title(x), self._axis_title(y)
+        x_title, y_title = self._axis_title(x_var), self._axis_title(y_var)
         main_df = main_df.sort_values(self.GROUP)
-        # main_df["csum"] = main_df["csum"] / 1_000_000
         fig = px.scatter(
             main_df,
-            x="csum",
-            y=y,
+            x="cumsum",
+            y=y_var,
             custom_data=["sc_point_gid", "print_capacity"],
             labels={
-                "csum": f"Cumulative {x_title}",
-                y: y_title,
+                "cumsum": f"Cumulative {x_title}",
+                y_var: y_title,
             },
             color=self.GROUP,
             color_discrete_sequence=px.colors.qualitative.Safe,
@@ -180,71 +165,57 @@ class Plots:
             unselected=dict(marker=dict(color="grey")),
         )
 
-        return self._update_fig_layout(fig, y)
+        return self._update_fig_layout(fig, y_var)
 
-    def binned(self, x, y, bins=100):
+    def binned(self, x_var, y_var, bins=100):
         """Return a line plot."""
         # The clustered scatter plot part
         main_df = None
         for key, df in self.datasets.items():
-            df = self._fix_doubles(df)
+            df = _fix_doubles(df)
             if main_df is None:
-                df = df.sort_values(x)
+                df = df.sort_values(x_var)
                 main_df = df.copy()
                 main_df[self.GROUP] = key
             else:
                 df[self.GROUP] = key
-                df = df.sort_values(x)
+                df = df.sort_values(x_var)
                 main_df = pd.concat([main_df, df])
 
-        # Assign bins as max bin value
-        main_df = main_df.dropna(subset=x)
-        main_df = main_df.sort_values(x)
-        minx, maxx = main_df[x].min(), main_df[x].max()
-        xrange = maxx - minx
-        bin_size = np.ceil(xrange / bins)
-        bins = np.arange(minx, maxx + bin_size, bin_size)
-
-        main_df["xbin"] = bins[-1]
-        for bn in bins[::-1]:
-            print(bn)
-            main_df["xbin"][main_df[x] <= bn] = bn
-
-        main_df["ybin"] = main_df.groupby(["xbin", self.GROUP])[y].transform(
-                                "mean"
-                          )
+        main_df = _add_bins(main_df, x_var, bins)
 
         # The simpler line plot part
-        main_df = main_df.sort_values([x, self.GROUP])
-        yagg = main_df.groupby(["xbin", self.GROUP])[y].transform("mean")
-        main_df["yagg"] = yagg
+        main_df = main_df.sort_values([x_var, self.GROUP])
+        grouped_by_x = main_df.groupby(["x_bin", self.GROUP])
+        main_df["y_agg"] = grouped_by_x[y_var].transform("mean")
         line_df = main_df.copy()
-        line_df = line_df[["xbin", "yagg", self.GROUP]].drop_duplicates()
+        line_df = line_df[["x_bin", "y_agg", self.GROUP]].drop_duplicates()
 
-        x_title, y_title = self._axis_title(x), self._axis_title(y)
+        x_title, y_title = self._axis_title(x_var), self._axis_title(y_var)
 
         # Points
         fig = px.scatter(
             main_df,
-            x="xbin",
-            y="yagg",  # Plot all y's so we can share selections with map
+            x="x_bin",
+            y="y_agg",  # Plot all y's so we can share selections with map
             custom_data=["sc_point_gid", "print_capacity"],
-            labels={x: x_title, y: y_title},
+            labels={x_var: x_title, y_var: y_title},
             color=self.GROUP,
             color_discrete_sequence=px.colors.qualitative.Safe,
         )
 
         # Lines
-        colors = px.colors.qualitative.Safe
-        for i, group in enumerate(line_df[self.GROUP].unique()):
+        for color, group in zip(
+            cycle(px.colors.qualitative.Safe), line_df[self.GROUP].unique()
+        ):
             df = line_df[line_df[self.GROUP] == group]
             lines = px.line(
                 df,
-                x="xbin",
-                y="yagg",
+                x="x_bin",
+                y="y_agg",
                 color=self.GROUP,
-                color_discrete_sequence=[colors[i]],
-            )  # <---------- We could run out of colors this way
+                color_discrete_sequence=color,
+            )
             fig.add_trace(lines.data[0])
 
         fig.layout["xaxis"]["title"]["text"] = x_title
@@ -255,13 +226,13 @@ class Plots:
             unselected=dict(marker=dict(color="grey")),
         )
 
-        return self._update_fig_layout(fig, y)
+        return self._update_fig_layout(fig, y_var)
 
-    def scatter(self, x, y):
-        """Return a regular scatterplot."""
+    def scatter(self, x_var, y_var):
+        """Return a regular scatter plot."""
         main_df = None
         for key, df in self.datasets.items():
-            df = self._fix_doubles(df)
+            df = _fix_doubles(df)
             if main_df is None:
                 main_df = df.copy()
                 main_df[self.GROUP] = key
@@ -269,16 +240,16 @@ class Plots:
                 df[self.GROUP] = key
                 main_df = pd.concat([main_df, df])
 
-        x_title, y_title = self._axis_title(x), self._axis_title(y)
+        x_title, y_title = self._axis_title(x_var), self._axis_title(y_var)
 
         main_df = main_df.sort_values(self.GROUP)
         fig = px.scatter(
             main_df,
-            x=x,
-            y=y,
+            x=x_var,
+            y=y_var,
             opacity=self.alpha,
             custom_data=["sc_point_gid", "print_capacity"],
-            labels={x: x_title, y: y_title},
+            labels={x_var: x_title, y_var: y_title},
             color=self.GROUP,
             color_discrete_sequence=px.colors.qualitative.Safe,
         )
@@ -289,13 +260,13 @@ class Plots:
             unselected=dict(marker=dict(color="grey")),
         )
 
-        return self._update_fig_layout(fig, y)
+        return self._update_fig_layout(fig, y_var)
 
-    def histogram(self, y, bins=100):
+    def histogram(self, y_var, bins=100):
         """Return a histogram."""
         main_df = None
         for key, df in self.datasets.items():
-            df = self._fix_doubles(df)
+            df = _fix_doubles(df)
             if main_df is None:
                 main_df = df.copy()
                 main_df[self.GROUP] = key
@@ -303,18 +274,18 @@ class Plots:
                 df[self.GROUP] = key
                 main_df = pd.concat([main_df, df])
 
-        y_title = self._axis_title(y)
+        y_title = self._axis_title(y_var)
         main_df = main_df.sort_values(self.GROUP)
 
         fig = px.histogram(
             main_df,
-            x=y,
-            labels={y: y_title},
+            x=y_var,
+            labels={y_var: y_title},
             color=self.GROUP,
             opacity=self.alpha,
             color_discrete_sequence=px.colors.qualitative.Safe,
             barmode="overlay",
-            nbins=bins
+            nbins=bins,
         )
 
         fig.update_traces(
@@ -322,13 +293,13 @@ class Plots:
             unselected=dict(marker=dict(color="grey")),
         )
 
-        return self._update_fig_layout(fig, y)
+        return self._update_fig_layout(fig, y_var)
 
-    def char_hist(self, x):
+    def char_hist(self, x_var):
         """Make a histogram of the characterization column."""
         main_df = list(self.datasets.values())[0]
         counts = {}
-        for str_dict in main_df[x]:
+        for str_dict in main_df[x_var]:
             if not isinstance(str_dict, str):
                 continue
             counts_for_sc_point = json.loads(str_dict)
@@ -345,7 +316,9 @@ class Plots:
             x="Category",
             y="Counts",
             labels={
-                "Category": self.config.titles.get(x, convert_to_title(x))
+                "Category": self.config.titles.get(
+                    x_var, convert_to_title(x_var)
+                )
             },
             opacity=self.alpha,
             color_discrete_sequence=px.colors.qualitative.Safe,
@@ -354,16 +327,16 @@ class Plots:
 
         return self._update_fig_layout(fig)
 
-    def box(self, y):
-        """Return a boxplot."""
+    def box(self, y_var):
+        """Return a box plot."""
 
         units = self.config.units.get(
-            DiffUnitOptions.remove_from_variable_name(y), ""
+            DiffUnitOptions.remove_from_variable_name(y_var), ""
         )
 
         def fix_key(key):
             """Display numbers and strings together."""
-            if is_integer(key):
+            if _is_integer(key):
                 key = str(key) + units
             return key
 
@@ -373,7 +346,7 @@ class Plots:
 
         main_df = None
         for key, df in dfs.items():
-            df = self._fix_doubles(df)
+            df = _fix_doubles(df)
             if main_df is None:
                 main_df = df.copy()
                 main_df[self.GROUP] = key
@@ -381,9 +354,9 @@ class Plots:
                 df[self.GROUP] = key
                 main_df = pd.concat([main_df, df])
 
-        y_title = self._axis_title(y)
+        y_title = self._axis_title(y_var)
 
-        if all(main_df[self.GROUP].apply(is_integer)):
+        if all(main_df[self.GROUP].apply(_is_integer)):
             main_df[self.GROUP] = main_df[self.GROUP].astype(int)
         main_df = main_df.sort_values(self.GROUP)
         main_df[self.GROUP] = main_df[self.GROUP].apply(fix_key)
@@ -391,9 +364,9 @@ class Plots:
         fig = px.box(
             main_df,
             x=self.GROUP,
-            y=y,
+            y=y_var,
             custom_data=["sc_point_gid", "print_capacity"],
-            labels={y: y_title},
+            labels={y_var: y_title},
             color=self.GROUP,
             color_discrete_sequence=px.colors.qualitative.Safe,
         )
@@ -409,26 +382,14 @@ class Plots:
             unselected=dict(marker=dict(color="grey")),
         )
 
-        return self._update_fig_layout(fig, y)
+        return self._update_fig_layout(fig, y_var)
 
-    def _fix_doubles(self, df):
-        """Check and or fix columns names when they match."""
-        if not isinstance(df, pd.core.frame.Series):
-            cols = np.array(df.columns)
-            counts = Counter(cols)
-            for col, count in counts.items():
-                if count > 1:
-                    idx = np.where(cols == col)[0]
-                    cols[idx[1]] = col + "_2"
-            df.columns = cols
-        return df
-
-    def _update_fig_layout(self, fig, y=None):
+    def _update_fig_layout(self, fig, y_var=None):
         """Update the figure layout with title, etc."""
         layout = copy.deepcopy(CHART_LAYOUT)
         layout["title"]["text"] = self.plot_title
         layout["legend_title_text"] = self.GROUP
         fig.update_layout(**layout)
-        if y:
-            fig.update_layout(yaxis={"range": self._plot_range(y)})
+        if y_var:
+            fig.update_layout(yaxis={"range": self._plot_range(y_var)})
         return fig
