35a7972bbc0064406a942657fb5d35d413b91be6
williamstravis <travis.williams@nrel.gov>
2022-06-03 13:30:50 -0600

    Added trailing comma in app.py.

--- reView/app.py
+++ reView/app.py
@@ -16,7 +16,7 @@ from reView.layout.layout import layout
 app = dash.Dash(
     __name__,
     suppress_callback_exceptions=True,
-    external_stylesheets=[dbc.themes.BOOTSTRAP]
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
 )
 app.layout = layout
 server = app.server
