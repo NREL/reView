27dcaa74ac6bc227d7ab23c4f0c4b9a88d9327a4
williamstravis <travis.williams@nrel.gov>
2022-06-03 21:33:32 -0600

    Trying download on a new branch.

--- reView/pages/scenario/view.py
+++ reView/pages/scenario/view.py
@@ -33,6 +33,7 @@ layout = html.Div(
     children=[
         # Path Name
         dcc.Location(id="/scenario_page", refresh=False),
+
         # Constant info block
         html.Div(
             [
@@ -62,6 +63,7 @@ layout = html.Div(
         ),
         REV_TOPTIONS_DIV,
         REV_PCA_DIV,
+
         # Map and chart
         html.Div(
             children=[
@@ -73,14 +75,19 @@ layout = html.Div(
         ),
         # To store option names for the map title
         html.Div(id="chosen_map_options", style={"display": "none"}),
+
         # To store option names for the chart title
         html.Div(id="chosen_chart_options", style={"display": "none"}),
+
         # For storing the data frame path and triggering updates
         html.Div(id="map_data_path", style={"display": "none"}),
+
         # For storing the signal need for the set of chart data frames
         html.Div(id="chart_data_signal", style={"display": "none"}),
+
         # Interim way to share data between map and chart
         html.Div(id="map_signal", style={"display": "none"}),
+
         # This table of recalc parameters
         html.Div(
             id="recalc_table",
@@ -102,9 +109,15 @@ layout = html.Div(
             ),
             style={"display": "none"},
         ),
+
         # Capacity after make_map (avoiding duplicate calls)
         html.Div(id="rev_mapcap", style={"display": "none"}),
+
         # Filter list after being pieced together
         html.Div(id="filter_store", style={"display": "none"}),
+
+        # Download
+        dcc.Download(id="download"),
+        html.Div(id="download_info", style={"display": "none"})
     ],
 )
