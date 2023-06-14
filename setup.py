9ae6557958c075bf1dffac9189de4a556d190ff6
Mike Gleason <mjgleaso@gmail.com>
2023-06-14 14:21:54 -0400

    refactoring dependencies to use environment.yml to allow for direct conda install of env

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@
 @author: travis
 """
 import os
+import re
 
 from setuptools import setup, find_packages
 
@@ -26,8 +27,28 @@ with open(os.path.join(REPO_DIR, "README.md"), encoding="utf-8") as f:
     README = f.read()
 
 with open("requirements.txt") as f:
-    INSTALL_REQUIREMENTS = f.read().splitlines()
+    INSTALL_REQUIREMENTS = f.readlines()
+with open("environment.yml") as f:
+    all_lines = [l.replace("\n", "").rstrip() for l in f.readlines()]
+deps_start_line = all_lines.index("dependencies:") + 1
+all_dep_lines = all_lines[deps_start_line:]
+all_deps = [l.lstrip().replace("- ", "") for l in all_dep_lines]
+if "pip:" in all_deps:
+    pip_start_line = all_deps.index("pip:")
+else:
+    pip_start_line = -1  # pylint: disable=invalid-name
+conda_deps = [
+    l for l in all_deps[:pip_start_line] if not l.startswith("python")
+]
+INSTALL_REQUIREMENTS += conda_deps
 
+SKIP_DEPS = []
+for skip_dep in SKIP_DEPS:
+    skip_matches = list(
+        filter(re.compile(skip_dep).match, INSTALL_REQUIREMENTS)
+    )
+    for skip_match in skip_matches:
+        INSTALL_REQUIREMENTS.pop(INSTALL_REQUIREMENTS.index(skip_match))
 
 setup(
     name="reView",
